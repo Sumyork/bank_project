97cb022f2683e9f84cdf74e7cdf949304a1b56a2
Sumyork <psychohistorian.p@gmail.com>
2023-09-06 23:32:30 +0300

    2/3

---
+++ src/main/java/com/example/bank_project/exception/AmountDebitedExceedsAmountAccount.java
@@ -0,0 +1,8 @@
+package com.example.bank_project.exception;
+
+public class AmountDebitedExceedsAmountAccount extends Exception {
+
+    public AmountDebitedExceedsAmountAccount() {
+        super();
+    }
+}
