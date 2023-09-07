0cb224eac69904e523a48fd8e91b7f683bfc9420
Sumyork <psychohistorian.p@gmail.com>
2023-09-08 00:59:02 +0300

    2/3

--- src/main/java/com/example/bank_project/BankProjectApplication.java
+++ src/main/java/com/example/bank_project/BankProjectApplication.java
@@ -1,17 +1,11 @@
 package com.example.bank_project;
 
-import com.example.bank_project.config.ConfigString;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class BankProjectApplication {
     public static void main(String[] args) {
-
-        AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext(ConfigString.class);
-
-
-        context.close();
+        SpringApplication.run(BankProjectApplication.class, args);
     }
 }
